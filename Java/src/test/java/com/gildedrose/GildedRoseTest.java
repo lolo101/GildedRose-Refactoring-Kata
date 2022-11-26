8296255b166ad1cd257d7ef9a233726231e7efe8
lolo101 <lbroquet@online.fr>
2022-11-26 21:23:56 +0100

    preliminary refactor

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -24,7 +24,7 @@ class GildedRoseTest {
 
     @Test
     void quality_should_not_go_below_zero() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(0, app.items[0].quality);
@@ -48,7 +48,7 @@ class GildedRoseTest {
 
     @Test
     void quality_should_not_go_over_fifty() {
-        Item[] items = new Item[] { new Item("Aged Brie", 1, 50) };
+        Item[] items = { new Item("Aged Brie", 1, 50) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(50, app.items[0].quality);
