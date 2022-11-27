d6b88457f52c76e69874af916d2cd9f57bbe0db1
lolo101 <lbroquet@online.fr>
2022-11-27 15:32:40 +0100

    merged conditions before and after sellIn decrement

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -19,6 +19,12 @@ class GildedRose {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
             }
+            item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 0) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
@@ -31,22 +37,16 @@ class GildedRose {
             if (item.sellIn < 6) {
                 item.quality = item.quality + 1;
             }
+            item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 0) {
+                item.quality = 0;
+            }
         } else {
             if (item.quality > 0) {
                 item.quality = item.quality - 1;
             }
-        }
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0) {
-            if (item.name.equals("Aged Brie")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                item.quality = 0;
-            } else {
+            item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 0) {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
                 }
